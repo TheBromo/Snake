b48c7d555709af9a2d4e5d030071c30958a54f74
TheBromo <manuel.strenge@hotmail.com>
2017-11-04 19:16:06 +0100

    More work done to the positioner and seed added.

--- test/src/ch/snake/Dot.java
+++ test/src/ch/snake/Dot.java
@@ -5,6 +5,12 @@ import java.util.Random;
 class Dot {
 
     private static int mX = 400, mY = 400, mSizeX = Draw.snakeSize, mSizeY = Draw.snakeSize;
+    Random r = new Random();
+
+    public Dot() {
+        r.setSeed(Lobby.seed);
+
+    }
 
     /**
      * Generates a new Position for the Dot that is not on the Snake head
@@ -12,7 +18,6 @@ class Dot {
     void changeToNewPosition() {
         //TODO Check all and the whole Snake
         //Generates New position
-        Random r = new Random();
         while (mX == SnakeHead.xHead && mY == SnakeHead.yHead || mX < 0 || mX > 760 || mY < 0 || mY > 760) {
             mX = (r.nextInt(41) * 20);
             mY = (r.nextInt(41) * 20);
--- test/src/ch/snake/Lobby.java
+++ test/src/ch/snake/Lobby.java
@@ -10,8 +10,8 @@ class Lobby {
     public static long seed;
     public static HashMap<InetAddress, Tail> users = new HashMap<InetAddress, Tail>();
 
-    public Lobby(String[] names, InetAddress[] ipAdresses,int size) {
-        seed=generateSeed(names);
+    public Lobby(String[] names, InetAddress[] ipAdresses, int size) {
+        seed = generateSeed(names);
         for (int i = 0; i < names.length; i++) {
             users.put(ipAdresses[i], new Tail(names[i]));
         }
@@ -20,9 +20,9 @@ class Lobby {
     private long generateSeed(String[] names) {
 
         Arrays.sort(names);
-        String s="";
-        for (int i=0; i<names.length; i++){
-            s=s.concat(names[i]);
+        String s = "";
+        for (int i = 0; i < names.length; i++) {
+            s = s.concat(names[i]);
         }
         long hash = 0;
         for (char c : s.toCharArray()) {
@@ -31,13 +31,20 @@ class Lobby {
         return hash;
     }
 
-    private void positionSetter(int size){
+    private void positionSetter(int size) {
         int playerCount = users.size();
-        if (playerCount==2){
+        if (playerCount==3){
+            playerCount=4;
+        }else if (playerCount>3&&playerCount<10){
+            playerCount=9;
+        }else if (playerCount>9){
+            playerCount=16;
+        }
+        if (playerCount == 2) {
             InetAddress[] keys;
             keys = users.keySet().toArray(new InetAddress[users.size()]);
-            users.get(keys[0]).setPos(size/4,size/2,keys[0]);
-            users.get(keys[1]).setPos((size/4)*3,size/2,keys[1]);
+            users.get(keys[0]).setPos(size / 4, size / 2, keys[0]);
+            users.get(keys[1]).setPos((size / 4) * 3, size / 2, keys[1]);
         }
 
 
