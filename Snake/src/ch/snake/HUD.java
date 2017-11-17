3b6c5e524aab1f4dba5c3fa1325e483de2318162
ManuelStrenge <manuel.strenge@hotmail.com>
2017-11-17 15:29:03 +0100

    Added License

--- Snake/src/ch/snake/HUD.java
+++ Snake/src/ch/snake/HUD.java
@@ -3,8 +3,26 @@ package ch.snake;
 import java.awt.*;
 import java.net.InetAddress;
 
+/**
+ * SwingSnake
+ * Copyright (C) 2017  Manuel Strenge
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 public class HUD {
-    public void showStats(Graphics g,Rectangle bounds){
+    public void showStats(Graphics g, Rectangle bounds){
         int shift = 15, counter = 0;
         //Displays all the names of the users in their according color
         for (InetAddress key : Lobby.getUsers().keySet()) {
--- Snake/src/ch/snake/Starter.java
+++ Snake/src/ch/snake/Starter.java
@@ -15,7 +15,24 @@ import java.util.Scanner;
 /**
  * @author Manuel Strenge https://github.com/TheBromo
  * @version v0.8
+ *
+ * SwingSnake
+ * Copyright (C) 2017  Manuel Strenge
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 public class Starter implements Discovery.Callback {
 
     ArrayList<InetAddress> inetAddresses=new ArrayList<>();
