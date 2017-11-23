ac72e9cb9ec54e997634434d2a7ac0a56664c5fd
ManuelStrenge <manuel.strenge@hotmail.com>
2017-11-23 21:59:44 +0100

    Started rework on network

--- Snake/src/ch/network/PacketBuilder.java
+++ Snake/src/ch/network/PacketBuilder.java
@@ -20,10 +20,11 @@ public class PacketBuilder {
         packet.setData(fillPacket(packet));
 
         checkNumber++;
-        return buffer;
+        return packet.getData();
     }
 
     public ByteBuffer fillPacket(Packet packet) throws UnknownHostException {
+
         HashMap<InetAddress, Tail> users = Lobby.getUsers();
         HashMap<InetAddress, Coordinates> heads = Lobby.getHeads();
         ByteBuffer data = packet.getData();
@@ -31,17 +32,48 @@ public class PacketBuilder {
         if (packet.getType() == PacketType.NAME) {
             data = buildNamePacket(data, users);
         } else if (packet.getType() == PacketType.COORDINATES) {
-
+            data = buildCoordinatesPacket(data, heads, users);
         } else if (packet.getType() == PacketType.DIRECTION) {
-
+            //TODO
         } else if (packet.getType() == PacketType.RESEND) {
-
+            //TODO
         } else if (packet.getType() == PacketType.CONNECTION) {
-
+            //TODO
         }
 
         return data;
     }
+
+    private ByteBuffer buildCoordinatesPacket(ByteBuffer data, HashMap<InetAddress, Coordinates> heads, HashMap<InetAddress, Tail> users) throws UnknownHostException {
+
+        // x, y, alive
+        Coordinates you = heads.get(InetAddress.getByName(InetAddress.getLocalHost().getHostAddress()));
+        data = addCoordinates(data, you);
+
+        boolean alive = users.get(InetAddress.getByName(InetAddress.getLocalHost().getHostAddress())).isAlive();
+        data = addBoolean(data, alive);
+
+        return data;
+    }
+
+    private ByteBuffer addCoordinates(ByteBuffer data, Coordinates coordinates) {
+
+        data.putInt(coordinates.newX);
+        data.putInt(coordinates.newY);
+
+        return data;
+    }
+
+    private ByteBuffer addBoolean(ByteBuffer data, boolean bool) {
+
+        if (bool) {
+            data.putInt(1);
+        } else {
+            data.putInt(0);
+        }
+        return data;
+    }
+
 
     private ByteBuffer buildNamePacket(ByteBuffer data, HashMap<InetAddress, Tail> users) throws UnknownHostException {
 
