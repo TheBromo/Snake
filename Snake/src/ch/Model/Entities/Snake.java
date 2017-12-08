5efc6fa7b26ae25eb80de673bccba590cfd649c0
ManuelStrenge <manuel.strenge@hotmail.com>
2017-12-08 23:43:31 +0100

    Started MVC

--- Snake/src/ch/Model/Entities/Snake.java
+++ Snake/src/ch/Model/Entities/Snake.java
@@ -1,4 +1,10 @@
 package ch.Model.Entities;
 
+import ch.Model.Utils.Coordinates;
+
+import java.util.ArrayList;
+
 public class Snake {
+    ArrayList<Coordinates> coordinates = new ArrayList<>();
+
 }
---
+++ Snake/src/ch/Model/Utils/Coordinates.java
@@ -0,0 +1,6 @@
+package ch.Model.Utils;
+
+public class Coordinates {
+    private int x, y;
+    private Direction direction;
+}
